53a02f77ae7ee5cf575aa26c0c85e82559b99947
bowiego <295030485@qq.com>
2026-07-26 17:32:03 +0800

    feat: remove native electron menubar

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,5 +1,5 @@
 import { electronApp, is, optimizer } from "@electron-toolkit/utils";
-import { app, BrowserWindow, ipcMain, shell } from "electron";
+import { app, BrowserWindow, ipcMain, Menu, shell } from "electron";
 import { join } from "path";
 import icon from "../../resources/icon.png?asset";
 
@@ -39,6 +39,9 @@ function createWindow(): void {
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
+	// Remove application menu so Alt key does nothing
+	Menu.setApplicationMenu(null);
+
 	// Set app user model id for windows
 	electronApp.setAppUserModelId("com.electron");
 
